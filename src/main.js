a827582cd175456e08de819f29eeed1a9e53d7b5
sky <yepwaibin@gmail.com>
2021-06-14 11:05:35 +0800

    用户列表功能

--- src/main.js
+++ src/main.js
@@ -6,6 +6,14 @@ import './assets/css/global.css'
 
 import axios from 'axios'
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+// axios请求拦截器
+axios.interceptors.request.use(config => {
+  // 为请求对象，添加Token验证的AUthorization字段
+  console.log(config)
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  // 在最后必需返回config
+  return config
+})
 Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
